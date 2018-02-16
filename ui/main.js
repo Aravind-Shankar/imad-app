df562e1168c2f4265797625370fbfd45919b986b
Aravind Shankar <shankaravind167@gmail.com>
2018-02-16 21:28:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,11 +38,20 @@ var submit = document.getElementById('submit_btn');
 var namelist = document.getElementById('namelist');
 
 submit.onclick = function() {
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == XMLHttpRequest.DONE)
+            if (request.status == 200) {
+                var names = JSON.parse(request.responseText);
+                var list = '';
+                
+                for (var i=0; i < names.length; ++i) {
+                    list += "<li>" + names[i] + "</li>";
+                }
+                namelist.innerHTML = list;
+            }
+    };
     
-    for (var i=0; i < names.length; ++i) {
-        list += "<li>" + names[i] + "</li>";
-    }
-    namelist.innerHTML = list;
+    request.open('GET', 'http://ee14b012.imad.hasura-app.io/submit-name?name=' + inName, true);
+    request.send(null);
 };
