a9ce7119ba4df9977e57d5ed4b56ed50b72c0932
Aravind Shankar <shankaravind167@gmail.com>
2018-02-16 20:42:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+var txt = document.getElementById('dynamic');
+txt.innerHTML = "GHOST EDIT!";
+
+var logo = document.getElementById('anim');
+var marginVal = 0;
+function moveLeft() {
+    marginVal = marginVal + 1;
+    logo.style.marginRight = marginVal;
+}
+
+logo.onclick = function() {
+    var interval = setInterval(moveLeft, 50);
+}
